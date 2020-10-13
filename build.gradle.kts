1549b0854f19a60e1e427b11e5e30c76fe3ef387
archinamon <archinamon@yandex.ru>
2020-10-13 12:38:42 +0300

    publishing; better src map

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     kotlin("multiplatform") version "1.4.10"
+    id("maven-publish")
 }
 
 group = "me.archinamon"
 version = "1.0"
+
+val os by lazy { org.gradle.internal.os.OperatingSystem.current()!! }
+val isRunningInIde: Boolean = System.getProperty("idea.active") == "true"
 
 repositories {
     mavenCentral()
@@ -18,9 +22,51 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when (hostOs) {
-        "Mac OS X" -> macosX64("native")
-        "Linux" -> linuxX64("native")
-//        isMingwX64 -> mingwX64("native") // not supported yet
+        "Mac OS X" -> macosX64("posix")
+        "Linux" -> linuxX64("posix")
+//        isMingwX64 -> mingwX64("windows") // not supported yet
         else -> throw GradleException("Host OS is not supported in File-IO project.")
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication> {
+        pom {
+            name.set("libui")
+            description.set("Kotlin/Native interop to libui: a portable GUI library")
+            url.set("https://github.com/Archinamon/native-file-io")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://github.com/Archinamon/native-file-io")
+                    distribution.set("repo")
+                }
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    id.set("Archinamon")
+                    name.set("Eduard Obolenskiy")
+                    email.set("archinamon@gmail.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/Archinamon/native-file-io")
+                connection.set("scm:git:https://github.com/Archinamon/native-file-io.git")
+                developerConnection.set("scm:git:git@github.com:Archinamon/native-file-io.git")
+            }
+        }
+    }
+
+    repositories {
+        maven("https://api.bintray.com/maven/archinamon/maven/native-file-io/;publish=0;override=1") {
+            credentials {
+                username = "Archinamon"
+                password = extra["apiKey"] as? String ?: ""
+            }
+        }
     }
 }
